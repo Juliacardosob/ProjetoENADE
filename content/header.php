3fa36a294d9956f8522cb4312814915d7f14845c
Gibran Khalil <135548432+GibranKhalil@users.noreply.github.com>
2023-11-01 14:37:14 -0300

    Criação da página painel

---
+++ content/header.php
@@ -0,0 +1,21 @@
+<?php
+include_once("body.php");
+?>
+<header>
+    <nav id="navbar">
+        <ul id="navList-container">
+            <li class="navbar-list">
+                <a href="#" class="navbar-link">Painel</a>
+            </li>
+            <li class="navbar-list">
+                <a href="#" class="navbar-link">Questões</a>
+            </li>
+            <li class="navbar-list">
+                <a href="#" class="navbar-link">Ranking</a>
+            </li>
+            <span class="space">
+            </span>
+            <li class="navbar-list"><a href="#" id="nav-button" class="navbar-link">Login/Cadastro<i class="material-icons">check_circle</i></a></li>
+        </ul>
+    </nav>
+</header>
