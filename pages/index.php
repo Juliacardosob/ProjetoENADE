a0079a1d87029906686272ee70e051df371eb215
Gibran Khalil <gibrankhalil1509@gmail.com>
2023-11-02 20:45:52 -0300

    Index 50%

--- pages/index.php
+++ pages/index.php
@@ -3,9 +3,38 @@ include_once("../content/header.php");
 ?>
 
 <div id="banner">
-        <div id="info-banner">
-            <p>Teste seu conhecimento</p>
-            <h1>Melhor plataforma de estudo para o ENADE</h1>
-            <button>Entrar para começar<i class="material-icons">arrow_right</i></button>
+    <img src="../img/enade.png" alt="" id="banner-img">
+    <div id="info-banner">
+        <p>Teste seu conhecimento</p>
+        <h1>Melhor plataforma de estudo para o ENADE</h1>
+        <button>Entrar para começar<i class="material-icons">arrow_right</i></button>
+    </div>
+</div>
+<div id="text-container">
+    <div id="infoText">
+        <h1>Bem-vindo ao centro de preparação para o ENADE</h1>
+        <h2>Um lugar perfeito para estudar com eficiência, conforto e confiança</h2>
+        <div id="display3">
+            <div class="icons-text">
+                <img src="../img/react.png" alt="" class="icons">
+                <p>Ciência da Computação</p>
+            </div>
+            <div class="icons-text">
+                <img src="../img/hardware.png" alt="" class="icons">
+                <p>Programação</p>
+            </div>
+            <div class="icons-text">
+                <img src="../img/programacao.png" alt="" class="icons">
+                <p>Hardware</p>
+            </div>
         </div>
+        <div id="indeximg">
+            <div class="imgindex-container">
+                <img src="../img/estudantes.jpg" alt="">
+            </div>
+            <div class="imgindex-container">
+                <img src="../img/solo.jpg" alt="">
+            </div>
+        </div>
+    </div>
 </div>
