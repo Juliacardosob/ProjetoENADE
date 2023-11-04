d9206398e52826eb82062196ed469f4ecbd8381a
Tales <tales.viana@hotmail.com>
2023-11-03 22:06:00 -0300

    Tela Ranking
    Tabela ranking, top 3 e pontos, erros e posição do aluno.

---
+++ pages/ranking.php
@@ -0,0 +1,103 @@
+<?php
+include_once("../content/body.php");
+?>
+
+<main class="ranking-container">
+
+    <div class="ranking-form"></div>
+    <!-- iNFORMAÇÕES PESSOAIS  -->
+    <div class="personal-stats">
+        <div class="card">Colocação:
+            <hr>
+            <div class="rank-number">
+                6°
+            </div>
+        </div>
+        <div class="card">Pontos:
+            <hr>
+            <div class="rank-number">
+                11
+            </div>
+        </div>
+        <div class="card">Erros:
+            <hr>
+            <div class="rank-number">
+                5
+            </div>
+        </div>
+    </div>
+
+    <hr style="margin: 50px;">
+
+    <!-- TRÊS MELHORES COLOCADOS COM IMG -->
+
+    <div class="top-ranked-form">
+        <div class="top-ranked-box">
+            <div class="top-ranked-img">
+                <img src="../img/logo.png">
+                <div class="top-ranked-number">
+                    1º
+                </div>
+            </div>
+            <hr>
+            <label>Zeca</label>
+        </div>
+
+        <div class="top-ranked-box">
+            <div class="top-ranked-img">
+                <img src="../img/logo.png">
+                <div class="top-ranked-number">
+                    2º
+                </div>
+            </div>
+            <hr>
+            <label>Lorem</label>
+        </div>
+
+        <div class="top-ranked-box">
+            <div class="top-ranked-img">
+                <img src="../img/logo.png">
+                <div class="top-ranked-number">
+                    3º
+                </div>
+            </div>
+            <hr>
+            <label>Lorem</label>
+        </div>
+
+    </div>
+
+    <hr style="margin: 50px">
+    <!-- TABELA DE RANKING -->
+    <div class="ranking-table">
+        <table>
+            <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Pontos</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Lorem</td>
+                <td>65</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Lorem</td>
+                <td>34</td>
+
+            </tr>
+            <tr>
+                <td>3</td>
+                <td>Lorem</td>
+                <td>20</td>
+            </tr>
+            <tr>
+                <td>4</td>
+                <td>Lorem</td>
+                <td>15</td>
+            </tr>
+        </table>
+    </div>
+
+</main>
