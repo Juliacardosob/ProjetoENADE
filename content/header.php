4ec107b41c2ec5bb8306dc8223f5bb0bffb8dd9e
Júlia Cardoso <74262172+Juliacardosob@users.noreply.github.com>
2023-11-05 20:23:51 -0300

    Cabeçalho com href
    links das páginas adicionados ao cabeçalho.

--- content/header.php
+++ content/header.php
@@ -5,17 +5,17 @@ include_once("body.php");
     <nav id="navbar">
         <ul id="navList-container">
             <li class="navbar-list">
-                <a href="#" class="navbar-link">Painel</a>
+                <a href="painel.php" class="navbar-link">Painel</a>
             </li>
             <li class="navbar-list">
                 <a href="#" class="navbar-link">Questões</a>
             </li>
             <li class="navbar-list">
-                <a href="#" class="navbar-link">Ranking</a>
+                <a href="ranking.php" class="navbar-link">Ranking</a>
             </li>
             <span class="space">
             </span>
-            <li class="navbar-list"><a href="#" id="nav-button" class="navbar-link">Login/Cadastro<i class="material-icons">check_circle</i></a></li>
+            <li class="navbar-list"><a href="login.php" id="nav-button" class="navbar-link">Login/Cadastro<i class="material-icons">check_circle</i></a></li>
         </ul>
     </nav>
 </header>
