ccfe07740f34d16eb5235fbba540ea89d445d1f1
Júlia Cardoso <74262172+Juliacardosob@users.noreply.github.com>
2023-11-07 15:04:08 -0300

    tentativa alterar cadastro

--- backend/alterar_cadastro.php
+++ backend/alterar_cadastro.php
@@ -1,38 +1,54 @@
 <?php
 include_once("conexao.php");
+include_once("../backend/entities/UserDAO.php");
 if(!isset($_SESSION)){
     session_start();
+}else{
+    $usuario = $_SESSION['usuario'];
+    $foto = $_SESSION['foto'];
 }
 
-if(isset($_SESSION['usuario'])){
-    if(isset($_POST['enviar'])){
-        if(isset($_FILES['mudarFoto']['name'])){
-            $foto = $_FILES['mudarFoto']['name'];
+//vê se tem foto no banco do usuário
+$select_f = $conn->prepare("SELECT * FROM aluno WHERE usuario = '$usuario'");
+$select_f->execute();
+$row = $select_f->fetch(PDO::FETCH_ASSOC);
+$resul = $row['foto'];
 
-            $usuario = $_SESSION['usuario'];
-            $caminho = "../img/fotos_perfil/'$usuario'";
+if(isset($_POST['enviar'])){
+    if(isset($_FILES['mudarFoto']['name'])){
+        $foto2 = $_FILES['mudarFoto'];
+        $fotonome = $_FILES['mudarFoto']['name'];
+        $fotonometemp = $_FILES['mudarFoto']['tmp_name'];
 
-            if(!realpath("../img/fotos_perfil/'$usuario'")){
-                mkdir($caminho, 0755, true);
-            }
-            
-            $move = move_uploaded_file($_FILES['mudarFoto']['tmp_name'], "$caminho/$foto");
-            if($move == true){
-                echo 'foto adicionada';
-                $inserir_bd = mysqli_query($mysqli, "UPDATE aluno SET foto = '$foto' WHERE usuario = '$usuario'"); //alterar no banco
-                $select_f = mysqli_query($mysqli, "SELECT * FROM aluno WHERE usuario = '$usuario'");
+        $caminho_p = "../img/fotos_perfil/'$usuario'";
 
-                $dados = mysqli_fetch_object($select_f);
-                echo "<img src='../img/fotos_perfil/'$usuario'/'$dados->foto''/>";
-
-            }
-        }else{
-            echo 'Por favor, selecione uma foto.';
+        if(!realpath($caminho_p)){
+            mkdir($caminho_p, 0755, true);
         }
-        mysqli_close($mysqli);
+
+        $move = move_uploaded_file($fotonometemp, "$caminho_p/$fotonome");
+
+        if($move == true){
+            $update_f = $conn->prepare("UPDATE aluno SET foto = '$fotonome' WHERE usuario = '$usuario'");
+            $result = $update_f->execute();
+
+            $User = new UserDAO($conn);
+            $newUser = new User();
+            $newUser->setFoto($foto2);
+            $User->definirVariaveisSessao($usuario, $foto2);
+            
+            if($result == true){
+                echo 'foto adicionada';
+            }
+        }
+    }else{
+        echo 'Por favor, selecione uma foto.';
     }
+}
+
+if($resul == null){
+    $caminho = '../img/';
 }else{
-    
-    echo 'Login na página não realizado';
+    $caminho = "../img/fotos_perfil/'$usuario'";
 }
 ?>
