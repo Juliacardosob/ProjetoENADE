3f0a7733722afbbc7ec5dd299983c011d34593b9
Júlia Cardoso <74262172+Juliacardosob@users.noreply.github.com>
2023-11-07 15:04:47 -0300

    atualização perfil

--- pages/perfil.php
+++ pages/perfil.php
@@ -17,7 +17,7 @@ if (!isset($_SESSION)) {
         <div class="perfil-box">
             <div class="perfil-conteudo">
                 <h1>Alterar cadastro</h1>
-                <img src="../img/<?=$foto?>" alt="fotoperfil" name="foto">
+                <img src="<?= $caminho;?><?= $foto;?>" alt="fotoperfil" name="foto">
                 <form method="post" enctype="multipart/form-data">
                     <button class="btn-form btn-foto" id="btnfoto">Mudar Foto</button>
                     <div class="file">
