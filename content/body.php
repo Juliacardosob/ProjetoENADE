aad46acbff70d229c859032408d9b6b7113b9312
Tales <tales.viana@hotmail.com>
2023-11-18 15:16:06 -0300

    Inclusão vlibras

--- content/body.php
+++ content/body.php
@@ -1,5 +1,5 @@
 <?php
-require_once ("../help/url.php");
+require_once("../help/url.php");
 
 
 ?>
@@ -16,9 +16,21 @@ require_once ("../help/url.php");
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-    
+
 </head>
+
 <body>
     <script src="../JS/script.js">
-        
+
+    </script>
+
+    <div vw class="enabled">
+        <div vw-access-button class="active"></div>
+        <div vw-plugin-wrapper>
+            <div class="vw-plugin-top-wrapper"></div>
+        </div>
+    </div>
+    <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
+    <script>
+        new window.VLibras.Widget('https://vlibras.gov.br/app');
     </script>
